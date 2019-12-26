33c13cb3b1722c3f0a9e1457be0a58716ae73b64
jeseias domingos <domingosjeseias@gmail.com>
2019-12-26 13:12:32 +0100

    Added the route to get all works

--- src/controllers/WorkController.js
+++ src/controllers/WorkController.js
@@ -1,6 +1,15 @@
 const Work = require('../models/Work')
 
 module.exports = {
+  async show (req, res) {
+    try {
+      const works = Work.find({})
+      return res.send(works)
+    } catch (error) {
+      
+    }
+  },
+
   async store (req, res) {
     try {
       const work = await Work.create(req.body)
--- src/routes.js
+++ src/routes.js
@@ -17,6 +17,7 @@ router.get('/user',  UserController.show)
 
 // Work
 router.post('/work', upload.single('thumbnail'), WorkController.store)
+router.get('/works', WorkController.show)
 
 // Message
 router.post('/msg', MsgController.store)
