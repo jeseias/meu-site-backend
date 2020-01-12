6ccdb5186d2c8235ca3af33d9ab14cb70ddb1c6a
jeseias domingos <domingosjeseias@gmail.com>
2020-01-12 23:09:30 +0100

    Chenges the index port file

--- src/database/index.js
+++ src/database/index.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose') 
 
-mongoose.connect('mongodb://localhost:27017/jmcd', {
+mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useFindAndModify: false
