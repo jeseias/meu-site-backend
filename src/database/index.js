8d55edcd95eaae884dccfbc9e644a46016568439
jeseias domingos <domingosjeseias@gmail.com>
2020-01-15 23:00:33 +0100

    Did the basic thing for my app to work

--- src/database/index.js
+++ src/database/index.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose') 
 
-mongoose.connect(process.env.MONGO_URI, {
+mongoose.connect(process.env.MONGO_URL, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useFindAndModify: false
