9812993953ebf335087a6cd80608227bcecfbec7
jeseias domingos <domingosjeseias@gmail.com>
2020-01-16 17:57:58 +0100

    Added files routes

--- src/models/Work.js
+++ src/models/Work.js
@@ -12,7 +12,7 @@ const WorkSchema = new mongoose.Schema({
 })
 
 WorkSchema.virtual('thumbnail_url').get(function(){
-  return `http://localhost:2001/files/${this.thumbnail}`
+  return `https://jmcd.herokuapp/${this.thumbnail}`
 })
 
 module.exports = mongoose.model('Trabalho', WorkSchema)
