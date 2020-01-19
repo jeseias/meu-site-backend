69be1a92fd2dca2d41df516e75ba5081f88899ea
jeseias domingos <domingosjeseias@gmail.com>
2020-01-19 13:25:08 +0100

    Changed the upload file strcuture

--- src/config/upload.js
+++ src/config/upload.js
@@ -1,14 +1,38 @@
-const multer = require('multer')
-const path = require('path')
+const multer = require('multer');
+const path = require('path');
+const crypto = require('crypto');
 
 module.exports = {
+  dest: path.resolve(__dirname, '..', '..', 'temp', 'upload'), 
   storage: multer.diskStorage({
-    destination: path.resolve(__dirname, '..', '..', 'uploads'),
-    filename: (req,file, cb) => {   
-      const ext = path.extname(file.originalname)
-      const name = path.basename(file.originalname,  ext) 
-      const imgNames = new String(`${name}-${Date.now()}${ext}`)
-      cb(null, `${imgNames.replace(/ /g,'')}`)
+    destination: (req, file, cb) => {
+      cb(null, path.resolve(__dirname, '..', '..', 'temp', 'upload'))
+    },
+    filename: (req, file, cb) => {
+      crypto.randomBytes(16, (err, hash) => {
+        if(err) cb(err); 
+
+        const fileName = `${hash.toString('hex')}-${file.originalname}`
+
+        cb(null, fileName)
+      })
+    },
+  }),
+  limits: {
+    fileSize: 3 * 1024 * 1024
+  },
+  fileFilter: (req, file, cb) => {
+    const allowedMimes = [
+      'image/jpeg',
+      'image/pjpeg',
+      'image/png',
+      'image/gif',
+    ];
+
+    if(allowedMimes.includes(file.mimetype)) {
+      cb(null, true);
+    } else {
+      cb(new Error('Invalid file type'));
     }
-  })
+  }
 }
