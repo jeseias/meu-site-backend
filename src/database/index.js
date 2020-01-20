59ccd741f9ad4247901abf023b734c37ad3a8802
jeseias domingos <domingosjeseias@gmail.com>
2020-01-20 13:39:40 +0100

    Changes the databases name

--- src/database/index.js
+++ src/database/index.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose') 
 
-mongoose.connect('mongodb+srv://jeseias:jeseias4475@cluster0-u6ro4.mongodb.net/test?retryWrites=true&w=majority', {
+mongoose.connect('mongodb+srv://jeseias:jeseias4475@cluster0-u6ro4.mongodb.net/jmcd?retryWrites=true&w=majority', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useFindAndModify: false
